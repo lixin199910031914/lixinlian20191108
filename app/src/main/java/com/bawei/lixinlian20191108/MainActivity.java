ef1d7e79754a629b144bb59492c0e74d8bc91474
lixin <1763346811@qq.com>
2019-11-08 19:40:39 +0800

    Initial commit

---
+++ app/src/main/java/com/bawei/lixinlian20191108/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bawei.lixinlian20191108;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
